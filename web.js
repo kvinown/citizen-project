a2995412d21d81c259701543ba94972af478a9a6
Kevin Owen <113487713+kvinown@users.noreply.github.com>
2024-05-12 18:09:43 +0700

    first demo

---
+++ web.js
@@ -0,0 +1,41 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+	let q = url.parse(req.url, true);
+	let path = q.query;
+	let fileLocation;
+
+	switch (path.menu) {
+		case "/":
+			fileLocation = "pages/home.html";
+			break;
+		case "home":
+			fileLocation = "pages/home.html";
+			break;
+		case "penduduk":
+			fileLocation = "pages/penduduk/index.html";
+			break;
+		case "Kartu Keluarga":
+			fileLocation = "pages/kartuKeluarga/index.html";
+			break;
+		default:
+			fileLocation = "pages/home.html";
+			break;
+	}
+
+	fs.readFile(fileLocation, (err, data) => {
+		if (err) {
+			res.writeHead(404, { "Content-type": "text/html" });
+			return res.end("404 not found");
+		}
+		res.writeHead(200, { "Content-type": "text/html" });
+		res.write(data);
+		return res.end();
+	});
+});
+
+server.listen(8888, () => {
+	console.log("Server is running on http://localhost:8888/");
+});
